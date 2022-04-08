125f86a578bbc7ea2a5c8a958e50e16787b970d1
muriloluz <muriloluz@inf.ufg.br>
2022-04-08 13:51:23 -0300

    Proposta nomenclatura de endpoints.

--- backend/src/main/java/br/ufg/inf/onboarding/repository/PessoaRepository.java
+++ backend/src/main/java/br/ufg/inf/onboarding/repository/PessoaRepository.java
@@ -2,12 +2,17 @@ package br.ufg.inf.onboarding.repository;
 
 import br.ufg.inf.onboarding.model.Pessoa;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RepositoryRestResource(path = "pessoa")
+@RepositoryRestResource(path = "pessoa", collectionResourceRel = "pessoas")
+@Description("Manutenção de pessoas")
 public interface PessoaRepository extends PagingAndSortingRepository<Pessoa,Integer> {
 
-    @RestResource(path = "cpf")
+    @RestResource(path = "byCpf", rel = "searchByCpf")
+    @Description("Retorna a pessoa com o CPF fornecido")
     Pessoa findPessoaByCpfEquals(String cpf);
 }
