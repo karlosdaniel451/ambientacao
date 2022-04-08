ed12a73510004790c394db35a34c1616c6cee7e2
muriloluz <muriloluz@inf.ufg.br>
2022-04-08 13:53:38 -0300

    Ajustando imports não utilizados.

--- backend/src/main/java/br/ufg/inf/onboarding/repository/PessoaRepository.java
+++ backend/src/main/java/br/ufg/inf/onboarding/repository/PessoaRepository.java
@@ -2,11 +2,9 @@ package br.ufg.inf.onboarding.repository;
 
 import br.ufg.inf.onboarding.model.Pessoa;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RepositoryRestResource(path = "pessoa", collectionResourceRel = "pessoas")
 @Description("Manutenção de pessoas")
