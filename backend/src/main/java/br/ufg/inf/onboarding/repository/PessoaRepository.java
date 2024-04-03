0df50614189556aca4b7f3008bb55c9d1e6fd306
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2024-04-03 15:59:44 -0300

    Adiciona busca de usuarios por nome no Backend

--- backend/src/main/java/br/ufg/inf/onboarding/repository/PessoaRepository.java
+++ backend/src/main/java/br/ufg/inf/onboarding/repository/PessoaRepository.java
@@ -1,6 +1,8 @@
 package br.ufg.inf.onboarding.repository;
 
 import br.ufg.inf.onboarding.model.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,4 +15,8 @@ public interface PessoaRepository extends PagingAndSortingRepository<Pessoa,Inte
     @RestResource(path = "byCpf", rel = "searchByCpf")
     @Description("Retorna a pessoa com o CPF fornecido")
     Pessoa findPessoaByCpfEquals(String cpf);
+
+    @RestResource(path = "byNome", rel="searchByNome")
+    @Description("Retorna as pessoas cujo nome contêm a string passada ignorando caso")
+    Page findAllByNomeContainsIgnoreCase(String nome, Pageable p);
 }
