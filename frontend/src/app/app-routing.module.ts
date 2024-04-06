159548fb1bdf0d6666e571624794ed903b52fc68
igorpadua <igorpadua@tutanota.com>
2024-04-05 21:01:45 -0300

    tela de adicionar usuario

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import {CriarPessoaComponent} from "./component/criar-pessoa/criar-pessoa.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'cria-pessoa',
+    pathMatch: 'full',
+  },
+  {
+    path: 'cria-pessoa',
+    component: CriarPessoaComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CriarPessoaComponent } from './component/criar-pessoa/criar-pessoa.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, CriarPessoaComponent],
+  imports: [BrowserModule, AppRoutingModule, MatInputModule, MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
